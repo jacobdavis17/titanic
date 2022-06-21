19efff06bbe456f44c83f74ac742e6feb97af580
Jacob Davis <jacobdavis777@gmail.com>
2022-06-21 12:23:33 -0700

    Added Ensemble of LR, RF, KNN, SVC; and manual Tree Bagging (best at 83.8%)

--- titanic.py
+++ titanic.py
@@ -4,17 +4,21 @@ Created on Fri Jun 17 14:07:50 2022
 
 @author: Jacob Davis
 """
-
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 from sklearn.impute import KNNImputer
 
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import LogisticRegression, RidgeClassifierCV, LogisticRegression
 from sklearn.metrics import accuracy_score
-
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
 
 #%% Import Data
 def importData():
@@ -40,13 +44,21 @@ def getAllNaNs(df):
     df_nan = df.isna()
     return df_nan 
 
+
+#%% Binning continuous features
+
+def binFeature(df, featureName):
+    
+    
+    
 #%% Dummify categoricals
 
 def dummifyCats(dfTrain, dfTest):
     
-    le = preprocessing.LabelEncoder()
     cols = ['Sex', 'Embarked']
     
+    le = preprocessing.LabelEncoder()
+        
     for col in cols:
         dfTrain[col] = le.fit_transform(dfTrain[col])
         dfTest[col] = le.transform(dfTest[col])
@@ -126,6 +138,10 @@ def splitData(df, test_size=0.2):
     
     return X_train, X_val, y_train, y_val
 
+# Random Forest param tester
+
+def randForParamTest(X_train, X_val, y_train, y_val):
+    
 
 #%% Main
 
@@ -151,23 +167,116 @@ if __name__ == '__main__':
     # Split train data into train and validation sets
     X_train, X_val, y_train, y_val = splitData(dfTrainImp, 0.2)
     
-    ### Train models ###
+    
+    
+#%% Train models ###
+    
+    # Logistic Regression
     lrc = LogisticRegression(random_state=0, max_iter=1000)
     lrc.fit(X_train, y_train)
+    predsLRC = lrc.predict(X_val)
+    print("LogReg accuracy: ", accuracy_score(y_val, predsLRC))
     
-    # Predict
-    preds = lrc.predict(X_val)
-    print("Validation accuracy: ", accuracy_score(y_val, preds))
+    #%%
+    # Random Forest parameter tester
+    paramName = 'min_samples_split'
+    paramVals = list(range(4,12))
+        
+    accuracy = np.zeros(len(paramVals))
+    rfc = RandomForestClassifier(random_state=0)
+    
+    for i, val in enumerate(paramVals):
+        param_grid = {paramName: [val]}
+        grid_search = GridSearchCV(estimator = rfc, param_grid = param_grid, 
+                              cv = 5, n_jobs = -1, verbose = 2)
+        grid_search.fit(X_train, y_train)
+        preds = grid_search.best_estimator_.predict(X_val)
+        accuracy[i] = accuracy_score(y_val, preds)
+    
+    plt.scatter(paramVals, accuracy)
+    
+    #%% Rand Forest Grid Search
+    
+    # Random Forest full grid search
+    param_grid = {
+            'bootstrap': [True],
+            'max_depth': [70, 75, 80],
+            'max_features': [2, 4],
+            'min_samples_leaf': [3, 4, 5],
+            'min_samples_split': [8, 11, 13],
+            'n_estimators': [75, 100, 125]
+            }
+       
+    rfc = RandomForestClassifier()
+    
+    # Instantiate the grid search model
+    grid_search = GridSearchCV(estimator = rfc, param_grid = param_grid, 
+                          cv = 5, n_jobs = -1, verbose = 2)
+    
+    grid_search.fit(X_train, y_train)
+    best_model = grid_search.best_estimator_
+    
+    predsRFC = best_model.predict(X_val)
+    print("RandFor accuracy: ", accuracy_score(y_val, predsRFC))
+    
+    #%% Ridge Classifier
+    
+    rc = RidgeClassifierCV()
+    rc.fit(X_train, y_train)
+    
+    predsRC = rc.predict(X_val)
+    print("RandFor accuracy: ", accuracy_score(y_val, predsRC))
     
     
+#%% XGBoost
+
+    #xgbc = XGBClassifier()
+    #xgbc.fit(X_train, y_train)
+    
+    #print(xgbc)
+    
+    #predsXGBC = xgbc.predict(X_val)
+    #print("RandFor accuracy: ", accuracy_score(y_val, predsXGBC))
     
     
+#%% Ensemble
+
+    log_clf = LogisticRegression(random_state=0)
+    rnd_clf = RandomForestClassifier(random_state=0)
+    knn_clf = KNeighborsClassifier()
+    svm_clf = SVC(random_state=0)
     
+    voting_clf = VotingClassifier(
+        estimators=[('lr', log_clf), ('rf', rnd_clf), ('svm', svm_clf)],
+        voting='hard')
+    #voting_clf.fit(X_train, y_train)
     
+    for clf in (log_clf, rnd_clf, svm_clf, knn_clf, voting_clf):
+        clf.fit(X_train, y_train)
+        pred = clf.predict(X_val)
+        print(clf.__class__.__name__, accuracy_score(y_val, pred))
     
+#%% Bagging (Bootstrap Aggregation)  
     
+    from sklearn.ensemble import BaggingClassifier
+    from sklearn.tree import DecisionTreeClassifier
     
+    n_est = list(range(70,126,5))
+    max_samp = list(range(175,226,5))
     
+    n = 1
+    tracker = dict()
+    for est in n_est:
+        for samp in max_samp:
+            
+            bag_clf = BaggingClassifier(
+                DecisionTreeClassifier(), n_estimators=est, max_samples=samp,
+                bootstrap=True, n_jobs=-1, random_state=0)
+    
+            bag_clf.fit(X_train, y_train)
+            pred = bag_clf.predict(X_val)
+            
+            tracker[(est,samp)] = accuracy_score(y_val, pred)
     
     
     
